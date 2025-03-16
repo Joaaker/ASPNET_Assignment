6afcf3681c9c419b9c56bfe690567b72dc266341
Joakim Erik Åkerman <joakim.akerman@hotmail.com>
2025-03-16 14:49:53 +0100

    WIP

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -49,3 +49,33 @@ const menu = document.getElementById('menu');
 hamburgerBtn.addEventListener('click', function() {
     menu.classList.toggle('active');
 });
+
+//Modal
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Open Buttons
+    const modalButtons = document.querySelectorAll('[data-modal="true"]')
+    modalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modalTarget = button.getAttribute('data-target')
+            const modal = document.querySelector(modalTarget)
+
+            if (modal)
+                modal.style.display = 'flex';
+        })
+    })
+
+
+    // Close Buttons
+    const closeButtons = document.querySelectorAll('[data-close="true"]')
+    closeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modal = button.closest('.modal-container')
+            if (modal) {
+                modal.style.display = 'none'
+
+                //Clear Formdata
+            }
+        })
+    })
+})
