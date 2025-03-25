3a783ccf8878d9ad2f432395813db11b1a59214f
Joakim Erik Åkerman <joakim.akerman@hotmail.com>
2025-03-25 17:11:31 +0100

    Updated Models

---
+++ Domain/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Models; 
+
+public class Member
+{
+    public string Id { get; set; } = null!;
+    //Member Image??
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string Email { get; set; } = null!;
+    public string? PhoneNumber { get; set; }
+    public string? JobTitle{ get; set; }
+    public DateOnly? DateOfBirth { get; set; }
+    public int? MemberAddressId { get; set; }
+}
---
+++ Domain/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Models;
+
+public class Project
+{
+    public string Id { get; set; } = null!;
+    //Project Image? 
+    //public IFormFile? ProjectImage { get; set; }
+    public string ProjectName { get; set; } = null!;
+    public string? Description { get; set; }
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public int? Budget { get; set; }
+    public string ClientName { get; set; } = null!;
+    public ICollection<int> MembersIds { get; set; } = [];
+}
